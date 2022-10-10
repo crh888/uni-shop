f2847a2f9d1f6ffb70d0d4c82461790ab7254f24
crh888 <nice235@163.com>
2022-10-10 23:22:47 +0800

    完成了商品列表页面的开发

--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,10 @@
 <template>
 	<view>
-		Goods_list
+		<view class="goods-list">
+      <view v-for="(goods, i) in goodsList" :key="i" @click="gotoDetail(goods)">
+        <my-goods :goods="goods"></my-goods>
+      </view>
+    </view>
 	</view>
 </template>
 
@@ -8,9 +12,64 @@
 	export default {
 		data() {
 			return {
-				
+        // 请求参数对象
+				queryObj: {
+          query: '',
+          cid: '',
+          pagnum: 1,
+          pagesize: 10
+        },
+        goodsList: [],
+        total: 0,
+       // 是否正在请求数据 (节流阀)
+       isLoading: false
+        
 			};
-		}
+		},
+    onLoad(options) {
+      this.queryObj.query = options.query || ''
+      this.queryObj.cid = options.cid || ''
+      this.getGoodsList()
+    },
+    methods: {
+      // 获取商品列表数据的方法
+      async getGoodsList(cb) {
+        // 打开节流阀
+        this.isLoading = true
+        const {data: res} = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
+        // 关闭节流阀
+        this.isLoading = false
+        // 自动关闭下拉刷新效果
+        cb && cb()
+        if (res.meta.status !== 200) return uni.$showMsg()
+        this.goodsList = [...this.goodsList, ...res.message.goods]
+        this.total = res.message.total
+      },
+      // 跳转到商品详情页
+      gotoDetail(goods) {
+        uni.navigateTo({
+          url: '/subpkg/goods_detail/goods_detail?goods_id=' + goods.goods_id
+        })
+      }
+    },
+    onReachBottom() {
+      // 判断是否还有下一页数据
+      if(this.queryObj.pagesize * this.queryObj.pagnum >= this.total) return uni.$showMsg('数据加载完毕')
+      // 判断是否有在发请求
+      if (this.isLoading) return
+      // 让页码值自增+1
+      this.queryObj.pagnum++
+      this.getGoodsList()
+    },
+    onPullDownRefresh() {
+      // 重置关键数据
+      this.queryObj.pagnum = 1,
+      this.total = 0,
+      this.isLoading = false,
+      this.goodsList = [],
+      // 重新发起请求
+      this.getGoodsList(() => {uni.stopPullDownRefresh()})
+    }
 	}
 </script>
 
